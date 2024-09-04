3d878d355d91adde4dce6380a2473dbcd397fd2b
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-04 15:46:37 +0300

    Commit message >>>>> working on the carousel <<<<<

--- frontend/src/Pages/HomePage/IndexPageStyles/IndexPageCarousel.jsx
+++ frontend/src/Pages/HomePage/IndexPageStyles/IndexPageCarousel.jsx
@@ -4,8 +4,12 @@ const IndexPageCarousel = () => {
     return (
         <Carousel>
             <Carousel.Item interval={1000}>
-
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
             </Carousel.Item>
+
         </Carousel>
     );
 };
