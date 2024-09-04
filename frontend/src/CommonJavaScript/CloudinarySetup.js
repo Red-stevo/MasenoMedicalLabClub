624fad33c6662836c5da8e8588d467472115e9ee
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-04 17:22:38 +0300

    Commit message >>>>> movng to the backend <<<<<

--- frontend/src/CommonJavaScript/CloudinarySetup.js
+++ frontend/src/CommonJavaScript/CloudinarySetup.js
@@ -1,11 +1,9 @@
-
 export const CloudinarySetup = cloudinary.createUploadWidget({
         cloudName: 'de91mnunt',
         uploadPreset: 'MasenoMedLabClub'}, (error, result) => {
         if (!error && result && result.event === "success") {
             console.log('Done! Here is the image info: ', result.info);
-        }else{
+        }else
             console.log(error)
-        }
     }
 )
