0634434ae783a104fd5f1d4a6420abe6fef7df8d
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-04 16:58:59 +0300

    Commit message >>>>> added the redux store <<<<<

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import './index.css'
+import {Provider} from "react-redux";
+import {store} from "./ReduxStorage/Store.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </StrictMode>,
 )
