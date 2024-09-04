bfcdb66009c91da98734db5a18b6418708f05237
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-04 17:20:02 +0300

    Commit message >>>>> worked on the cloudinary setup <<<<<

---
+++ frontend/src/CommonJavaScript/CloudinarySetup.js
@@ -0,0 +1,11 @@
+
+export const CloudinarySetup = cloudinary.createUploadWidget({
+        cloudName: 'de91mnunt',
+        uploadPreset: 'MasenoMedLabClub'}, (error, result) => {
+        if (!error && result && result.event === "success") {
+            console.log('Done! Here is the image info: ', result.info);
+        }else{
+            console.log(error)
+        }
+    }
+)
