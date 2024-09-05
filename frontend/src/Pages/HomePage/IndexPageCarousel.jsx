1ce2c6fdf3822fc4778b11744466352b10a3a04e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-05 16:04:18 +0300

    Commit message >>>>> done for th day <<<<<

--- frontend/src/Pages/HomePage/IndexPageCarousel.jsx
+++ frontend/src/Pages/HomePage/IndexPageCarousel.jsx
@@ -1,14 +1,36 @@
 import {Carousel, Image} from "react-bootstrap";
-import "./IndexPageStyles/IndexPageCarousel.css"
+import "./IndexPageStyles/IndexPageCarousel.css";
+import image1 from "./IndexPageAssets/img1.png"
+import image2 from "./IndexPageAssets/img2.png"
+import image3 from "./IndexPageAssets/img3.png"
 
 const IndexPageCarousel = () => {
     return (
         <Carousel id={"image-holder"}>
             <Carousel.Item interval={1000}>
-                <Image alt={"Meseno Med Lab Science Index Page Image."}/>
+                <Image src={image1} alt={"Meseno Med Lab Science Index Page Image."} height={500}
+                       className={"carousel-image"} />
+                <Carousel.Caption className={"caption-text"}>
+                    <h3>Image title</h3>
+                    <p>Image description</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item interval={1000}>
+                <Image src={image2} alt={"Meseno Med Lab Science Index Page Image."} height={500}
+                       className={"carousel-image"} />
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3>Image title</h3>
+                    <p>Image description</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item interval={1000}>
+                <Image src={image3} alt={"Meseno Med Lab Science Index Page Image."} height={500}
+                       className={"carousel-image"}/>
+                <Carousel.Caption>
+                    <h3>Image title</h3>
+                    <p>Image description</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
--- frontend/src/Pages/HomePage/IndexPageHeader.jsx
+++ frontend/src/Pages/HomePage/IndexPageHeader.jsx
@@ -1,5 +1,5 @@
 import "./IndexPageStyles/IndexPageHeader.css"
-import {Image, Nav, Navbar, NavItem, NavLink} from "react-bootstrap";
+import {Image, Nav, Navbar, NavItem} from "react-bootstrap";
 import masenoLogo from "./IndexPageAssets/MasenoSchoolLogo.jpg";
 import { ReactTyped } from "react-typed";
 
@@ -17,9 +17,20 @@ const IndexPageHeader = () => {
                         typeSpeed={180} backSpeed={40} backDelay={3} loop
                         className={"color-effect"}/>
                     </NavItem>
-                    <NavItem>
-                        <NavLink href={"#"} id={"signin-link"} >Signin</NavLink>
-                    </NavItem>
+                    <Nav variant={"tabs"} defaultActiveKey={"/"} className={"nav-sections"}>
+                        <NavItem>
+                            <Nav.Link href={"/"} id={"signin-link"}>Home</Nav.Link>
+                        </NavItem>
+                        <NavItem>
+                            <Nav.Link href={"#"} id={"signin-link"}>About Us</Nav.Link>
+                        </NavItem>
+                        <NavItem>
+                            <Nav.Link href={"#"} id={"signin-link"}>Contact Us</Nav.Link>
+                        </NavItem>
+                        <NavItem>
+                            <Nav.Link href={"#"} id={"signin-link"}>Signin</Nav.Link>
+                        </NavItem>
+                    </Nav>
                 </Nav>
         </Navbar>
     );
