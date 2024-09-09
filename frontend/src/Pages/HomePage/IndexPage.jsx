a7951245db730c31ee9562fda8e50f0462f2c0c2
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-09 09:22:22 +0300

    Commit message >>>>> added the contact us page. <<<<<

--- frontend/src/Pages/HomePage/IndexPage.jsx
+++ frontend/src/Pages/HomePage/IndexPage.jsx
@@ -2,6 +2,7 @@ import "./IndexPageStyles/IndexPage.css";
 import IndexPageHeader from "./IndexPageHeader.jsx";
 import IndexPageCarousel from "./IndexPageCarousel.jsx";
 import IndexPageAboutUs from "./IndexPageAboutUs.jsx";
+import IndexPageContactUs from "./IndexPageContactUs.jsx";
 const IndexPage = () => {
     return (
         <div className={"index-page-sections"}>
@@ -11,6 +12,7 @@ const IndexPage = () => {
             <div className={"index-body"} >
                 <IndexPageCarousel />
                 <IndexPageAboutUs />
+                <IndexPageContactUs />
             </div>
         </div>
     )
---
+++ frontend/src/Pages/HomePage/IndexPageContactUs.jsx
@@ -0,0 +1,16 @@
+import "./IndexPageStyles/IndexPageContactUs.css"
+const IndexPageContactUs = () => {
+    return (
+        <div>
+            <h2 className={"contact-us-header"}>Contact US</h2>
+
+            <div className={"icons-section"}>
+                <div></div>
+                <div></div>
+            </div>
+
+        </div>
+    );
+};
+
+export default IndexPageContactUs;
