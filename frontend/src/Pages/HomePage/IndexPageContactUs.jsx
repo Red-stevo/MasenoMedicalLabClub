5ae207a58f00b9335a5b6ce3dd32295f1f27eaaf
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-09 11:36:36 +0300

    Commit message >>>>> alined the icons <<<<<

--- frontend/src/Pages/HomePage/IndexPageContactUs.jsx
+++ frontend/src/Pages/HomePage/IndexPageContactUs.jsx
@@ -15,37 +15,38 @@ const IndexPageContactUs = () => {
             <div className={"icons-section"}>
                 <div className={"left-icons"}>
                     <Link to={"https://www.instagram.com/maseno.mlssa?igsh=MW1iZWs4emJxZzdnag=="}
-                    target={"_blank"} rel={"no-opener no-referrer"} className={"instagram-icon"}>
+                    target={"_blank"} rel={"no-opener no-referrer"} className={"icon"}>
                         <Image src={instagramIcon} alt={"Instagram icon."} height={150} width={150}/>
                         <span>Follow Us on Instagram</span>
                     </Link>
 
                     <Link to={"https://m.facebook.com/groups/487454420299782/?ref=share&mibextid=NSMWBT"}
-                          target={"_blank"} rel={"no-opener no-referrer"} className={"facebook-icon"}>
+                          target={"_blank"} rel={"no-opener no-referrer"} className={"icon"}>
                         <Image src={facebookIcon} alt={"facebook icon"} height={150} width={150}/>
-                        <span>Connect with Us on Facebook</span>
+                        <span>Follow Us on Facebook</span>
                     </Link>
 
                     <Link to={"https://x.com/Maseno_MLSSA?t=pMaP4iNzKPwFhR8QDSyGOQ&s=09"}
-                          target={"_blank"} rel={"no-opener no-referrer"}>
+                          target={"_blank"} rel={"no-opener no-referrer"}  className={"icon"}>
                         <Image src={xIcon} alt={"X Icon"} height={150} width={150}></Image>
                         <span>Follow Us on X</span>
                     </Link>
                 </div>
                 <div className={"right-icon"}>
                     <Link to={"https://www.linkedin.com/groups/9849659"} target={"_blank"}
-                          rel={"no-opener no-referrer"}>
+                          rel={"no-opener no-referrer"}  className={"icon"}>
                         <Image src={linkedInIcon} alt={"LinkedIn icon"} height={150} width={150}/>
                         <span>Find Us on LinkedIn</span>
                     </Link>
 
                     <Link to={"https://chat.whatsapp.com/FfRnluR5acgHWeJhgBanY5"} target={"_blank"}
-                          rel={"no-opener no-referrer"}>
+                          rel={"no-opener no-referrer"}  className={"icon"}>
                         <Image src={whatsAppIcon} alt={"WhatsApp Icon"} height={150} width={150}/>
                         <span>Contact Us via WhatsApp</span>
                     </Link>
 
-                    <Link to={"mailto:maseno.mlssa@gmail.com"} target={"_blank"} rel={"no-opener no-referrer"}>
+                    <Link to={"mailto:maseno.mlssa@gmail.com"} target={"_blank"} rel={"no-opener no-referrer"}
+                          className={"icon"}>
                         <Image src={gmailIcon} alt={"gmail icon"} height={150} width={150}/>
                         <span>Email Us at Gmail</span>
                     </Link>
