20233a4783710fb24ffee354808c92b4e175b04d
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-10 14:49:08 +0300

    Commit message >>>>> update file structure <<<<<

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import IndexPage from "./Pages/HomePage/IndexPage.jsx";
+import IndexPage from "./Pages/IndexPage/IndexPage.jsx";
 
 function App() {
     return (
