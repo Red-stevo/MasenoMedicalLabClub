dc9226356d9fc16708e95ba993f3c1c25b43ccbe
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-10 15:19:03 +0300

    Commit message >>>>> worked on the footer and the desktop view is all done <<<<<

---
+++ frontend/src/Pages/IndexPage/Components/IndexFooter.jsx
@@ -0,0 +1,11 @@
+import "./../IndexPageStyles/IndexFooter.css"
+
+const IndexFooter = () => {
+    return (
+        <footer className={"index-footer"}>
+            <span>&copy; 2024 MMLSSA.All rights reserved</span>
+        </footer>
+    );
+};
+
+export default IndexFooter;
