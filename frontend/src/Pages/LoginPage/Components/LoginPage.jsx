a0ce2c742db78d3f816fd0719504a9223f8f7694
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-11 11:28:13 +0300

    Commit message >>>>> working on the login page. <<<<<

---
+++ frontend/src/Pages/LoginPage/Components/LoginPage.jsx
@@ -0,0 +1,23 @@
+import "./../LoginPageStyles/LoginPage.css";
+const LoginPage = () => {
+    return (
+        <div className={"login-page"}>
+            <div className={"login-section"}>
+                <div className={"login-header"}>
+                    Header.
+                </div>
+
+                <div className={"login-body"}>
+                    Body.
+                </div>
+
+                <div className={"login-footer"}>
+                    Footer.
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
