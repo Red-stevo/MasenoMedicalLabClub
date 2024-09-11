3afee72a1d734af2ed9b123bb5356d04d38c1fe3
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-11 12:30:15 +0300

    Commit message >>>>> working on the login page. <<<<<

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import IndexPage from "./Pages/IndexPage/Components/IndexPage.jsx";
+import LoginPage from "./Pages/LoginPage/Components/LoginPage.jsx";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path={"/"} element={<IndexPage />}>
-
-                </Route>
+                <Route path={"/"} element={<IndexPage />}></Route>
+                <Route path={"/login"} element={<LoginPage />}></Route>
             </Routes>
         </Router>
     );
--- frontend/src/Pages/LoginPage/Components/LoginPage.jsx
+++ frontend/src/Pages/LoginPage/Components/LoginPage.jsx
@@ -1,14 +1,25 @@
 import "./../LoginPageStyles/LoginPage.css";
+import {Form, FormGroup} from "react-bootstrap";
 const LoginPage = () => {
     return (
         <div className={"login-page"}>
             <div className={"login-section"}>
                 <div className={"login-header"}>
-                    Header.
+                    MMLSA Login
                 </div>
 
                 <div className={"login-body"}>
-                    Body.
+                    <Form className={"login-form"}>
+                        <Form.Group className={"email-group"}>
+                            <Form.Label>Email : </Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+
+                        <Form.Group className={"password-group"}>
+                            <Form.Label>Password : </Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+                    </Form>
                 </div>
 
                 <div className={"login-footer"}>
