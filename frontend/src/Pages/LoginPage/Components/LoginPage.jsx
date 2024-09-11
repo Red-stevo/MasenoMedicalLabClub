97b9c98f5fa707c935bdf06fec7c5ab58301f056
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-11 13:23:38 +0300

    Commit message >>>>> worked on the login page for desktop view <<<<<

--- frontend/src/Pages/LoginPage/Components/LoginPage.jsx
+++ frontend/src/Pages/LoginPage/Components/LoginPage.jsx
@@ -1,5 +1,6 @@
 import "./../LoginPageStyles/LoginPage.css";
-import {Form, FormGroup} from "react-bootstrap";
+import {Form} from "react-bootstrap";
+import {Link} from "react-router-dom";
 const LoginPage = () => {
     return (
         <div className={"login-page"}>
@@ -11,19 +12,19 @@ const LoginPage = () => {
                 <div className={"login-body"}>
                     <Form className={"login-form"}>
                         <Form.Group className={"email-group"}>
-                            <Form.Label>Email : </Form.Label>
-                            <Form.Control />
+                            <Form.Label htmlFor={"email"} className={"text-font"}>Email : </Form.Label>
+                            <Form.Control id={"email"} />
                         </Form.Group>
 
                         <Form.Group className={"password-group"}>
-                            <Form.Label>Password : </Form.Label>
-                            <Form.Control />
+                            <Form.Label htmlFor={"password"} className={"text-font"}>Password : </Form.Label>
+                            <Form.Control id={"password"} />
                         </Form.Group>
                     </Form>
                 </div>
 
                 <div className={"login-footer"}>
-                    Footer.
+                    <Link className={"visitor-link"} to={"/"}>Continue as Visitor</Link>
                 </div>
 
             </div>
