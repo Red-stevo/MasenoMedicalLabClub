c5c6aa1bf5f00a8cff6dc4a0d6dcf383f290af53
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-15 08:26:09 +0300

    Commit message >>>>> added exception handling in a better way. <<<<<

--- MasenoMedLabClub/src/main/java/red/stevo/code/masenomedlabclub/ControllerAdvice/SecurityExceptions/AccessDeniedExceptionHandler.java
+++ MasenoMedLabClub/src/main/java/red/stevo/code/masenomedlabclub/ControllerAdvice/SecurityExceptions/AccessDeniedExceptionHandler.java
@@ -1,24 +1,23 @@
 package red.stevo.code.masenomedlabclub.ControllerAdvice.SecurityExceptions;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
-import java.io.IOException;
-
-@RequiredArgsConstructor
 @Component("AccessDeniedExceptionHandler")
 public class AccessDeniedExceptionHandler implements AccessDeniedHandler {
-
     private final HandlerExceptionResolver resolver;
+
+    public AccessDeniedExceptionHandler(@Qualifier("handlerExceptionResolver")HandlerExceptionResolver resolver){
+        this.resolver = resolver;
+    }
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
-                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                       AccessDeniedException accessDeniedException) {
         resolver.resolveException(request, response, null, accessDeniedException);
     }
 }
