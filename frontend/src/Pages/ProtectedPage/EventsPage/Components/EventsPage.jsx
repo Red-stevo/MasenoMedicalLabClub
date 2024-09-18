90d52b91718a30beecdb9316dbccadb36b8fa7ac
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-18 12:06:09 +0300

    Commit message >>>>> events page update. <<<<<

--- frontend/src/Pages/ProtectedPage/EventsPage/Components/EventsPage.jsx
+++ frontend/src/Pages/ProtectedPage/EventsPage/Components/EventsPage.jsx
@@ -1,62 +1,164 @@
 import "./../Styles/EventsPage.css";
 import {Button, Card} from "react-bootstrap";
 import {IoAddSharp} from "react-icons/io5";
+import {Link} from "react-router-dom";
 const EventsPage = () => {
     return (
         <div className={"events-page"}>
-            <Card className={"events-card"}>
-                <Card.Img variant="top" src="holder.js/80px80"/>
-                <Card.Body>
-                    <Card.Text>
-                        Some quick bulk of the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
             <br/>
-            <Card className={"events-card"}>
-                <Card.Img variant="top" src="holder.js/100px180"/>
-                <Card.Body>
-                    <Card.Text>
-                        Some quick bulk of the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
             <br/>
-            <Card className={"events-card"}>
-                <Card.Img variant="top" src="holder.js/100px180"/>
-                <Card.Body>
-                    <Card.Text>
-                        Some quick bulk of the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
             <br/>
-            <Card className={"events-card"}>
-                <Card.Img variant="top" src="holder.js/80px80"/>
-                <Card.Body>
-                    <Card.Text>
-                        Some quick bulk of the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
             <br/>
-            <Card className={"events-card"}>
-                <Card.Img variant="top" src="holder.js/100px180"/>
-                <Card.Body>
-                    <Card.Text>
-                        Some quick bulk of the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
             <br/>
-            <Card className={"events-card"}>
-                <Card.Img variant="top" src="holder.js/100px180"/>
-                <Card.Body>
-                    <Card.Text>
-                        Some quick bulk of the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
+            <br/>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
+            <br/>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
+            <br/>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
+            <br/>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
+            <br/>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
+            <br/>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
+            <br/>
+
+            <Link to={"#"} className={"events-card"}>
+                <Card className={"events-card"}>
+                    <Card.Img variant="top" src="holder.js/80px80"/>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
 
             <Button className={"add-event"}><IoAddSharp /> Event</Button>
         </div>
