927d5f47f6afc72ac92fc00d4396eb7eae783deb
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-18 12:42:14 +0300

    Commit message >>>>> add events page form <<<<<

--- frontend/src/Pages/ProtectedPage/EventsPage/Components/AddEventPage.jsx
+++ frontend/src/Pages/ProtectedPage/EventsPage/Components/AddEventPage.jsx
@@ -1,10 +1,26 @@
-import {Form} from "react-bootstrap";
+import {FloatingLabel, Form} from "react-bootstrap";
 import "./../Styles/AddEventPage.css";
 
 const AddEventPage = () => {
     return (
         <Form className={"add-event-form"}>
-            <h2>Add Event</h2>
+            <Form.Group>
+                <Form.Label>Events Title : </Form.Label>
+                <Form.Control type={"text"} />
+            </Form.Group>
+
+            <FloatingLabel controlId="floatingTextarea" label="Event Decription">
+                <Form.Control
+                    as="textarea"
+                    placeholder="Event Description"
+                    style={{ height: '100px' }}
+                />
+            </FloatingLabel>
+
+            <Form.Group>
+                <Form.Label>Event Images : </Form.Label>
+                <Form.Control type={"file"} />
+            </Form.Group>
         </Form>
     );
 };
