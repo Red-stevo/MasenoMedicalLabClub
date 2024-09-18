a2c38c54121338f54cdddff30229cb55a1675cac
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-18 10:50:46 +0300

    Commit message >>>>> event card display <<<<<

--- frontend/src/Pages/ProtectedPage/EventsPage/Components/EventsPage.jsx
+++ frontend/src/Pages/ProtectedPage/EventsPage/Components/EventsPage.jsx
@@ -1,8 +1,67 @@
 import "./../Styles/EventsPage.css";
+import {Card} from "react-bootstrap";
 const EventsPage = () => {
     return (
         <div className={"events-page"}>
-            <h2>Events page.</h2>
+            <Card className={"events-card"}>
+                <Card.Img variant="top" src="holder.js/80px80"/>
+                <Card.Body>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <br/>
+            <Card className={"events-card"}>
+                <Card.Img variant="top" src="holder.js/100px180"/>
+                <Card.Body>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <br/>
+            <Card className={"events-card"}>
+                <Card.Img variant="top" src="holder.js/100px180"/>
+                <Card.Body>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <br/>
+            <Card className={"events-card"}>
+                <Card.Img variant="top" src="holder.js/80px80"/>
+                <Card.Body>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <br/>
+            <Card className={"events-card"}>
+                <Card.Img variant="top" src="holder.js/100px180"/>
+                <Card.Body>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <br/>
+            <Card className={"events-card"}>
+                <Card.Img variant="top" src="holder.js/100px180"/>
+                <Card.Body>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
