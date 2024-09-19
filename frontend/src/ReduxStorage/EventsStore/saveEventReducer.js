e4b28a3d7dab5e1f2a3dd02d27bfa6318cccc155
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-19 11:59:20 +0300

    Commit message >>>>> upfated axios configuration. <<<<<

--- frontend/src/ReduxStorage/EventsStore/saveEventReducer.js
+++ frontend/src/ReduxStorage/EventsStore/saveEventReducer.js
@@ -1,4 +1,6 @@
 import {createAsyncThunk, createEntityAdapter, createSlice} from "@reduxjs/toolkit";
+import secureAxios from "../../DataSourceConfig/secureAxios.js";
+import axiosConfig from "../../DataSourceConfig/axiosConfig.js";
 
 
 const eventsDataAdapter = createEntityAdapter();
@@ -13,6 +15,7 @@ const saveEvent = createAsyncThunk("save-event/new-event",
     (eventData, config) => {
 
     try {
+        const response = axiosConfig.post();
 
     }catch (error){
         return config.rejectWithValue(error.response.data.message);
