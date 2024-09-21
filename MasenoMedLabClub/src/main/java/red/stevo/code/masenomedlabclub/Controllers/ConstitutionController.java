9c7b2072dc6f69d9779dd3d726db43f45717bbea
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-09-21 19:21:58 +0300

    simple reformation

--- MasenoMedLabClub/src/main/java/red/stevo/code/masenomedlabclub/Controllers/ConstitutionController.java
+++ MasenoMedLabClub/src/main/java/red/stevo/code/masenomedlabclub/Controllers/ConstitutionController.java
@@ -18,32 +18,32 @@ public class ConstitutionController {
     private final ConstitutionService constitutionService;
 
     // Create a section
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<?> createSection(@RequestBody List<ConstitutionSectionRequest> sections) {
         return new ResponseEntity<>(constitutionService.createSection(sections), HttpStatus.CREATED);
     }
 
     // Get all sections
-    @GetMapping
+    @GetMapping("get")
     public ResponseEntity<List<ConstitutionSection>> getAllSections() {
         return ResponseEntity.ok(constitutionService.getAllSections());
     }
 
     // Get a specific section by ID
-    @GetMapping("/{id}")
+    @GetMapping("/get/{id}")
     public ResponseEntity<ConstitutionSection> getSectionById(@PathVariable Long id) {
         return ResponseEntity.ok(constitutionService.getSectionById(id));
     }
 
     // Update a section by ID
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<?> updateSection(@PathVariable Long id,
                                            @RequestBody ConstitutionSectionRequest section) {
         return ResponseEntity.ok(constitutionService.updateSection(id, section));
     }
 
     // Delete a section by ID
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteSection(@PathVariable Long id) {
         constitutionService.deleteSection(id);
         return ResponseEntity.ok("deleted successfully");
