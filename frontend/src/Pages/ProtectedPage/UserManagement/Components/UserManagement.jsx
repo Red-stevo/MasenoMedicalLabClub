995f811fb744997ad8c4db479cc94e330030ea21
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-25 19:37:55 +0300

    Commit message >>>>> users display page. <<<<<

--- frontend/src/Pages/ProtectedPage/UserManagement/Components/UserManagement.jsx
+++ frontend/src/Pages/ProtectedPage/UserManagement/Components/UserManagement.jsx
@@ -3,7 +3,21 @@ import "./../Styles/UserManagement.css";
 const UserManagement = () => {
     return (
         <div className={"user-management-page"}>
-            <h2>User Management Page.</h2>
+            <div className={"user-display-title title-user-management"}>
+                <div>ID</div>
+                <div>Email</div>
+                <div>Position</div>
+                <div>Role</div>
+            </div>
+            <div className={"users-holder"}>
+                <div className={"user-display-title users-view"}>
+                    <div>1</div>
+                    <div>stephen.muiru22@students.dkut.ac.ke</div>
+                    <div>ChairPerson</div>
+                    <div>Admin</div>
+                </div>
+
+            </div>
         </div>
     );
 };
