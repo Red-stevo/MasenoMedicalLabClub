18404b40d84c17aad04a30261bdb33ea002bb1c2
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-26 17:08:17 +0300

    Commit message >>>>> working on getting the users. <<<<<

--- frontend/src/Pages/ProtectedPage/UserManagement/Components/UserManagement.jsx
+++ frontend/src/Pages/ProtectedPage/UserManagement/Components/UserManagement.jsx
@@ -1,7 +1,19 @@
 import "./../Styles/UserManagement.css";
 import {Button, Form} from "react-bootstrap";
+import {useForm} from "react-hook-form";
+import {useState} from "react";
 
 const UserManagement = () => {
+    const [usersState, setUsersState] = useState([]);
+    const [editUserState, setEditUserState] = useState(null);
+    const {register, handleSubmit, reset
+    } = useForm();
+
+    const handleAddUsers = (data) => {
+
+    }
+
+
     return (
         <div className={"user-management-page"}>
             <div className={"user-display-title title-user-management"}>
@@ -10,7 +22,8 @@ const UserManagement = () => {
                 <div>Position</div>
                 <div>Role</div>
             </div>
-            <div className={"users-holder"}>
+
+            <div className={"users-holder"} onDoubleClick={() => {}}>
                 <div className={"user-display-title users-view"}>
                     <div>1</div>
                     <div>stephen.muiru22@students.dkut.ac.ke</div>
@@ -20,9 +33,11 @@ const UserManagement = () => {
 
                 <Form className={"user-reg-form"}>
                     <span className={"space"}></span>
-                    <input className={"form-control email-input"} placeholder={"Email e.g. jameskago@gmail.com"} />
+                    <input className={"form-control email-input"} placeholder={"Email e.g. jameskago@gmail.com"}
+                           {...register("email") }/>
 
-                    <select className={"form-select position-select"} defaultValue={"Member"}>
+                    <select className={"form-select position-select"} defaultValue={"Member"}
+                            {...register("position")}>
                         <option value={"Chair Person"}>Chair Person</option>
                         <option value={"Vise Chair Person"}>Vise Chair Person</option>
                         <option value={"Treasure"}>Treasure</option>
@@ -32,14 +47,15 @@ const UserManagement = () => {
                         <option value={"Member"}>Member</option>
                     </select>
 
-                    <select className={"form-select role-select"} defaultValue={"USER"}>
+                    <select className={"form-select role-select"} defaultValue={"USER"}
+                            {...register("role")}>
                         <option value={"USER"}>USER</option>
                         <option value={"ADMIN"}>ADMIN</option>
                     </select>
 
-                    <Button className={"apply-button"}>Apply</Button>
                 </Form>
-                    <Button className={"cancel-changes-button"}>Cancel</Button>
+                <Button className={"apply-button"}>Apply</Button>
+                <Button onClick={() => window.location.reload()} className={"cancel-changes-button"}>Cancel</Button>
             </div>
         </div>
     );
