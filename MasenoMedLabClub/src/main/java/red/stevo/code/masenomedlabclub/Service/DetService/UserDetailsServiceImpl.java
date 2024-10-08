bfa72962711ce41229cb90d7002fc385204d34bb
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-10-08 11:00:07 +0300

    Commit message >>>>> backend updates <<<<<

--- MasenoMedLabClub/src/main/java/red/stevo/code/masenomedlabclub/Service/DetService/UserDetailsServiceImpl.java
+++ MasenoMedLabClub/src/main/java/red/stevo/code/masenomedlabclub/Service/DetService/UserDetailsServiceImpl.java
@@ -18,11 +18,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Users userInfo = repository.findByEmail(email).orElseThrow(() -> {
-            return new UserDoesNotExistException("User Does Not Exist.");
-        });
-
-        return userInfo;
+    public UserDetails loadUserByUsername(String email){
+        return repository.findByEmail(email).orElseThrow(() -> new UserDoesNotExistException("User Does Not Exist."));
     }
 }
