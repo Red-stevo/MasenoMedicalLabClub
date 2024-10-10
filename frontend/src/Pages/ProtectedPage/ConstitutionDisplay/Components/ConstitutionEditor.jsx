bb4f965946644c6ddc378495f56a0cd7023ec2bc
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-10-10 18:37:50 +0300

    Commit message >>>>> basic text editor <<<<<

--- frontend/src/Pages/ProtectedPage/ConstitutionDisplay/Components/ConstitutionEditor.jsx
+++ frontend/src/Pages/ProtectedPage/ConstitutionDisplay/Components/ConstitutionEditor.jsx
@@ -1,10 +1,22 @@
 import "../Styles/ConstitutionEditor.css";
+import {useState} from "react";
+import {Editable, Slate, withReact} from "slate-react";
+import {createEditor} from "slate";
 
-
+const initialValue = [
+    {
+        type: 'paragraph',
+        children: [{ text: 'A line of text in a paragraph.' }],
+    },
+]
 const ConstitutionEditor = () => {
+    const [editor] = useState(() => withReact(createEditor()));
+
     return (
         <div className={"constitution-editor-page"}>
-
+            <Slate editor={editor} initialValue={initialValue}>
+                <Editable />
+            </Slate>
         </div>
     );
 };
