d892d03f5e9a6c7e449a1c90db12c822c60cc8b1
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-10-14 20:49:32 +0300

    Commit message >>>>> comment on section, added the research section <<<<<

--- frontend/src/Pages/UserProfile/Components/UserProfilePage.jsx
+++ frontend/src/Pages/UserProfile/Components/UserProfilePage.jsx
@@ -9,57 +9,46 @@ const UserProfilePage = () => {
                <div className={"profile-image-background"}>
                    <FcAddImage className={"add-image-button"}/>
                    <Image  alt={"Profile image"}  roundedCircle className={"profile-image image-holder"}
-                           src="https://res.cloudinary.com/de91mnunt/image/upload/v1728578611/events_images_folder/qy1gnamxtoc4hq0vcvcw.png"/>
+                         src="https://res.cloudinary.com/de91mnunt/image/upload/v1728578611/events_images_folder/qy1gnamxtoc4hq0vcvcw.png"/>
                </div>
+                <Card className={"profile-card"}>
+                    <Card.Body className={"profile-card-body"}>
+                        <Card.Title className={"user-name"}>Stephen Muiru</Card.Title>
+                        <Card.Title className={"reg-no"}>C026-01-0931/2022</Card.Title>
+                    </Card.Body>
+                </Card>
+
+                {/*Contact me display section*/}
                 <div>
-                    <Card className={"profile-card"}>
-                        <Card.Body className={"profile-card-body"}>
-                            <Card.Title className={"user-name"}>Stephen Muiru</Card.Title>
-                            <Card.Title className={"reg-no"}>C026-01-0931/2022</Card.Title>
-                        </Card.Body>
-                    </Card>
-
-                    <div>
-                        <div className={"contact-me-edit-holder"}>
-                            <h5 className={"contact-me-header"}>Contact Me</h5><LiaEdit className={"edit-icon"}/>
+                    <div className={"contact-me-edit-holder"}>
+                        <h5 className={"contact-me-header"}>Contact Me</h5><LiaEdit className={"edit-icon"}/>
+                    </div>
+                    <div className={"contact-me-body"}>
+                        <div className={"contact-me-image-holder"}>
+                            <Image variant="top" className={"image-holder contact-image"}
+                                   src="https://res.cloudinary.com/de91mnunt/image/upload/v1728578611/events_images_folder/qy1gnamxtoc4hq0vcvcw.png"/>
+                            <h6>WhatsApp</h6>
                         </div>
-                        <div className={"contact-me-body"}>
-                            <div className={"contact-me-image-holder"}>
-                                <Image variant="top" className={"image-holder contact-image"}
-                                       src="https://res.cloudinary.com/de91mnunt/image/upload/v1728578611/events_images_folder/qy1gnamxtoc4hq0vcvcw.png"/>
-                                <h6>WhatsApp</h6>
-                            </div>
-                            <div className={"contact-me-image-holder"}>
-                                <Image variant="top" className={"image-holder contact-image"}
-                                       src="https://res.cloudinary.com/de91mnunt/image/upload/v1728578611/events_images_folder/qy1gnamxtoc4hq0vcvcw.png"/>
-                                <h6>WhatsApp</h6>
-                            </div>
-                            <div className={"contact-me-image-holder"}>
-                                <Image variant="top" className={"image-holder contact-image"}
-                                       src="https://res.cloudinary.com/de91mnunt/image/upload/v1728578611/events_images_folder/qy1gnamxtoc4hq0vcvcw.png"/>
-                                <h6>WhatsApp</h6>
-                            </div>
-                            <div className={"contact-me-image-holder"}>
-                                <Image variant="top" className={"image-holder contact-image"}
-                                       src="https://res.cloudinary.com/de91mnunt/image/upload/v1728578611/events_images_folder/qy1gnamxtoc4hq0vcvcw.png"/>
-                                <h6>WhatsApp</h6>
-                            </div>
-                            <div className={"contact-me-image-holder"}>
-                                <Image variant="top" className={"image-holder contact-image"}
-                                       src="https://res.cloudinary.com/de91mnunt/image/upload/v1728578611/events_images_folder/qy1gnamxtoc4hq0vcvcw.png"/>
-                                <h6>WhatsApp</h6>
-                            </div>
-                            <div className={"contact-me-image-holder"}>
-                                <Image variant="top" className={"image-holder contact-image"}
-                                       src="https://res.cloudinary.com/de91mnunt/image/upload/v1728578611/events_images_folder/qy1gnamxtoc4hq0vcvcw.png"/>
-                                <h6>WhatsApp</h6>
-                            </div>
-
+                        <div className={"contact-me-image-holder"}>
+                            <Image variant="top" className={"image-holder contact-image"}
+                                   src="https://res.cloudinary.com/de91mnunt/image/upload/v1728578611/events_images_folder/qy1gnamxtoc4hq0vcvcw.png"/>
+                            <h6>WhatsApp</h6>
+                        </div>
+                        <div className={"contact-me-image-holder"}>
+                            <Image variant="top" className={"image-holder contact-image"}
+                                   src="https://res.cloudinary.com/de91mnunt/image/upload/v1728578611/events_images_folder/qy1gnamxtoc4hq0vcvcw.png"/>
+                            <h6>WhatsApp</h6>
                         </div>
                     </div>
                 </div>
-            </div>
 
+                {/*User research display section*/}
+                <div>
+                    <div>
+                        <h5 className={"research-header"}>My Research</h5>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
